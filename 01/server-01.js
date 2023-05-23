90197474fd4b60655d25582991d41f805c1446d5
ehuntober <ehuntercrypto@gmail.com>
2023-05-23 12:15:09 +0000

    new file added

--- 01/server-01.js
+++ 01/server-01.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
 
+const api =  require('./api')
 const port = process.env.PORT || 1337
 
 const app = express()
@@ -15,6 +16,7 @@ async function listProducts (req, res){
 
     try{
         const data = await fs.readFile(productsFile)
+        res.setHeader('Access-Control-Allow-Origin')
         res.join(JSON.parse(data))
 
     }
